a0639117ab0849ab8c470beb07775e8592613bd6
moose0030 <matthewpreston@hotmail.ca>
2015-09-11 01:24:50 -0400

    -added test for displaying all cards of a hand

--- workspace/A1-SimplePoker-TDD/src/HandTest.java
+++ workspace/A1-SimplePoker-TDD/src/HandTest.java
@@ -5,8 +5,15 @@ import org.junit.Test;
 public class HandTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetHand(){
+		Card[] cards = new Card[5];
+		
+		for(int i=0;i<5;i++){
+			Card c = new Card("TwoSpades");
+			cards[i]=c;
+		}
+		Hand h = new Hand(cards);
+		assertEquals("TwoSpades,TwoSpades,TwoSpades,TwoSpades,TwoSpades",h.getHand());
 	}
 
 }
