059ccdc5dc7da1134fecd8731adc188a01d240ea
moose0030 <matthewpreston@hotmail.ca>
2015-09-11 01:17:39 -0400

    -added Card constructor with string parameter -added getSuit and getRank functions

--- workspace/A1-SimplePoker-TDD/src/CardTest.java
+++ workspace/A1-SimplePoker-TDD/src/CardTest.java
@@ -7,13 +7,14 @@ public class CardTest {
 	@Test
 	public void testRank() {
 		Card c = new Card("TwoSpades");
-		assertEquals(2,c.getRank());
+		assertEquals("Two",c.getRank());
 	}
 	
 	@Test
 	public void testSuit() {
 		Card c = new Card("TwoSpades"); 
 		assertEquals("Spades",c.getSuit());
+		//System.out.println(c.)
 	}
 
 }
