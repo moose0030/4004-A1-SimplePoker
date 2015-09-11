3f5c4c9bd4bb76c52cd2270f7ea4cabeb92049c2
moose0030 <matthewpreston@hotmail.ca>
2015-09-11 01:49:57 -0400

    -fixed test for displaying hand -created test/code for displaying # of one type of card in hand

--- workspace/A1-SimplePoker-TDD/src/HandTest.java
+++ workspace/A1-SimplePoker-TDD/src/HandTest.java
@@ -7,13 +7,26 @@ public class HandTest {
 	@Test
 	public void testGetHand(){
 		Card[] cards = new Card[5];
+		for(int i=0;i<5;i++){
+			Card c = new Card("TwoSpades");
+			cards[i]=c;
+		}
 		
+		Hand h = new Hand(cards);
+		System.out.println(h.getHand());
+		assertEquals("TwoSpades,TwoSpades,TwoSpades,TwoSpades,TwoSpades",h.getHand());
+		
+	}
+	@Test
+	public void testNumberTwos(){	
+		Card[] cards = new Card[5];
 		for(int i=0;i<5;i++){
 			Card c = new Card("TwoSpades");
 			cards[i]=c;
 		}
 		Hand h = new Hand(cards);
-		assertEquals("TwoSpades,TwoSpades,TwoSpades,TwoSpades,TwoSpades",h.getHand());
+		
+		assertEquals(5,h.printDups(2));
 	}
 
 }
