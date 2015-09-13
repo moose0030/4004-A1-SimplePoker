29c49c5c4ceeb50bfcc5c813d281289828e28606
moose0030 <matthewpreston@hotmail.ca>
2015-09-13 19:48:56 -0400

    - added outline to Game that will ultimately handle input and output of game

--- workspace/A1-SimplePoker-TDD/src/Game.java
+++ workspace/A1-SimplePoker-TDD/src/Game.java
@@ -1,4 +1,33 @@
+import java.io.IOException;
 
 public class Game {
 
+	
+	public static void main(String[] args) {
+		
+		System.out.println("How many players are playing this round?");
+		int num = 0;
+		while(num < 2){
+			try {
+				System.in.read();
+				num = 2;
+			} 
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		for(int i=0; i<num;i++){
+			//while()
+			System.out.println("Please enter your id and your cards");
+			try {
+				System.in.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
 }
