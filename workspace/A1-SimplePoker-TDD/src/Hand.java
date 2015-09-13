860f74b9858b470b9ae8abb86fd581ead130917f
moose0030 <matthewpreston@hotmail.ca>
2015-09-13 16:30:31 -0400

    - added test and code for determining if hand is royal flush

--- workspace/A1-SimplePoker-TDD/src/Hand.java
+++ workspace/A1-SimplePoker-TDD/src/Hand.java
@@ -1,4 +1,72 @@
 
 public class Hand {
 
+	public Hand(Card[] c){
+		cards = c;
+		for(int i =0; i<c.length;i++){
+			switch(c[i].getRank()){
+			case "Two":duplicates[0]++;break;
+			case "Three":duplicates[1]++;break;
+			case "Four":duplicates[2]++;break;
+			case "Five":duplicates[3]++;break;
+			case "Six":duplicates[4]++;break;
+			case "Seven":duplicates[5]++;break;
+			case "Eight":duplicates[6]++;break;
+			case "Nine":duplicates[7]++;break;
+			case "Ten":duplicates[8]++;break;
+			case "Jack":duplicates[9]++;break;
+			case "Queen":duplicates[10]++;break;
+			case "King":duplicates[11]++;break;
+			case "Ace":duplicates[12]++;break;
+			}
+		}
+	}
+	
+	public String getHand(){
+		String value = "";
+		for(int i=0; i<cards.length;i++){
+			value += cards[i].getRank();
+			value += cards[i].getSuit();
+			if(i<cards.length-1)
+				value += ",";
+		}
+		return value;
+	}
+	
+	//Two is 0, Ace is 12
+	public int printDups(int n){
+		if(n>1 && n<(duplicates.length+3))
+		  return duplicates[n-2];
+		return 0;
+	}
+	
+	private int[] duplicates = new int[13];
+	private Card[] cards;
+	public String getPokerHand() {
+		
+		//royal flush
+		System.out.println("" + duplicates[12] + duplicates[11]+duplicates[10]+duplicates[9]+duplicates[8]);
+		if(duplicates[12] == 1 && duplicates[11] == 1 && duplicates[10] == 1 && duplicates[9] == 1 && duplicates[8] == 1){
+			if(cards[0].getSuit() == cards[1].getSuit() && cards[0].getSuit() == cards[2].getSuit() && cards[0].getSuit() == cards[3].getSuit() && cards[0].getSuit() == cards[4].getSuit())
+				return "Royal Flush";
+		}
+		return "";
+		//straight flush
+		
+		//4OAK
+		
+		//Full house
+		
+		//flush
+		
+		//straight
+		
+		//3OAK
+		
+		//2Pair
+		
+		//1Pair
+		
+		//HighCard
+	} 
 }
--- workspace/A1-SimplePoker-TDD/src/HandTest.java
+++ workspace/A1-SimplePoker-TDD/src/HandTest.java
@@ -28,5 +28,19 @@ public class HandTest {
 		
 		assertEquals(5,h.printDups(2));
 	}
+	@Test
+	public void testRoyalFlush(){
+		Card a = new Card("AceSpades");
+		Card k = new Card("KingSpades");
+		Card q = new Card("QueenSpades");
+		Card j = new Card("JackSpades");
+		Card t = new Card("TenSpades");
+		
+		Card[] cards = {a,k,q,j,t};
+		
+		
+		Hand h = new Hand(cards);
+		assertEquals("Royal Flush",h.getPokerHand());
+	}
 
 }
