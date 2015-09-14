c6647b0b5979b68dadb38b8605820fe5f43af85b
moose0030 <matthewpreston@hotmail.ca>
2015-09-13 20:04:43 -0400

    - added Round constructor and addPlayerHand fucntion with tests

--- workspace/A1-SimplePoker-TDD/src/Round.java
+++ workspace/A1-SimplePoker-TDD/src/Round.java
@@ -1,4 +1,13 @@
 
 public class Round {
-
+	Player[] players;
+	
+	public Round(int n){
+		players = new Player[n];
+	}
+	
+	public void addPlayerHand(int id, Card[] cards){
+		players[id] = new Player(String.valueOf(id),new Hand(cards));
+	}
+	
 }
--- workspace/A1-SimplePoker-TDD/src/RoundTest.java
+++ workspace/A1-SimplePoker-TDD/src/RoundTest.java
@@ -5,8 +5,12 @@ import org.junit.Test;
 public class RoundTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddPlayerHand() {
+		Round r = new Round(2);
+		for(int i=0;i<2;i++){
+			r.addPlayerHand(i,new Card[]{new Card("ThreeSpades"),new Card("ThreeSpades"),new Card("ThreeSpades"),new Card("ThreeSpades"),new Card("ThreeSpades")});
+		}
+		assertEquals(2,r.players.length);
 	}
 
 }
