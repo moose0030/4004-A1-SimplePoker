dc402f42014e315f4417312624c38a1e00d92f51
moose0030 <matthewpreston@hotmail.ca>
2015-09-13 22:39:32 -0400

    - finished basic flow of game i/o

--- workspace/A1-SimplePoker-TDD/src/Game.java
+++ workspace/A1-SimplePoker-TDD/src/Game.java
@@ -14,7 +14,9 @@ public class Game {
 		}
 		
 		r = new Round(num);
-		
+		boolean playAgain = true;
+		while(playAgain){
+			playAgain= false;
 		for(int i=0; i<num;i++){
 			String s = "";
 			System.out.println("Please enter your id and your cards");
@@ -24,13 +26,17 @@ public class Game {
 			}
 			String[] split = s.split("\\s+");
 			r.addPlayerHand(Integer.valueOf(split[0]),new Card[]{new Card(split[1]),new Card(split[2]),new Card(split[3]),new Card(split[4]),new Card(split[5])});
-			
-			
-			
 		}
 		
+		// r.printPokerHand
 		for(int j=0; j<r.players.length;j++){
-		System.out.println(r.players[j].getHand().getHand());	
+		System.out.println(r.players[j].getHand().getPokerHand());	
 	}
+		System.out.println("Would you like to play another round? y/n");
+		if(reader.nextLine().equals("y"))
+			playAgain = true;
+				
+		}
+		System.out.println("Game Over");
 	}
 }
