4feddd18228e840546030167948fe04ed1b5229c
moose0030 <matthewpreston@hotmail.ca>
2015-09-25 14:22:20 -0400

    added Tie output condition, added test, and fixed broken tests

--- workspace/3004-A1/src/Round.java
+++ workspace/3004-A1/src/Round.java
@@ -11,14 +11,33 @@ public class Round {
 		players[i] = new Hand(cards, s);
 	}
 
-	public String betterHand(Hand[] hands) {
-		Arrays.sort(hands);
+	public String betterHand(Hand[] h) {
+		Arrays.sort(h);
 		String s = "";
 		int i = 1;
-		for (Hand h : hands) {
+		for (int j =0; j<h.length;j++){
+			if(j+1<h.length){
+				if(h[j].compareTo(h[j+1])== 0){
+					s += "Tie" + " | " + h[j].getID() + " | " + h[j].getHand() + " | " + h[j].getPokerHand() + " " + h[j].getHandRank() + "\n";
+				
+				}
+				else{
+					s += i + " | " + h[j].getID() + " | " + h[j].getHand() + " | " + h[j].getPokerHand() + " " + h[j].getHandRank() + "\n";
+				}
+						
+					i++;
+			}
+		
+			else if(j>0){
+				if(h[j].compareTo(h[j-1])== 0)
+					s += "Tie" + " | " + h[j].getID() + " | " + h[j].getHand() + " | " + h[j].getPokerHand() + " " + h[j].getHandRank() + "\n";
+				else
+					s += i + " | " + h[j].getID() + " | " + h[j].getHand() + " | " + h[j].getPokerHand() + " " + h[j].getHandRank() + "\n";
+				i++;
+			}
+		
 			
-			s += i + " | " + h.getID() + " | " + h.getHand() + " | " + h.getPokerHand() + " " + h.getHandRank() + "\n";
-			i++;
+			//i++;
 		}
 		s = s.trim();
 		return s;
--- workspace/3004-A1/src/RoundTest.java
+++ workspace/3004-A1/src/RoundTest.java
@@ -23,6 +23,7 @@ public class RoundTest {
 				new Card("ThreeSpades"), new Card("TwoSpades") });
 		Hand[] hs = new Hand[] { h1, h2 };
 		// need to fix for straigh and flush
+		System.out.println(r.betterHand(hs));
 		assertEquals(
 				"1 | null | TwoSpades TwoSpades ThreeHearts ThreeSpades TwoSpades | Full House 7\n2 | null | TwoSpades AceSpades ThreeSpades ThreeSpades TwoSpades | Flush 5",
 				r.betterHand(hs));
@@ -36,13 +37,22 @@ public class RoundTest {
 		Hand h2 = new Hand(new Card[] { new Card("FourSpades"), new Card("FourSpades"), new Card("ThreeHearts"),
 				new Card("ThreeSpades"), new Card("FourSpades") });
 		Hand[] hs = new Hand[] { h1, h2 };
-		// System.out.println(r.betterHand(hs));
-		// System.out.println("Test\n1 | null | FourSpades FourSpades
-		// ThreeHearts ThreeSpades FourSpades | Full House 7 4\n2 | null |
-		// TwoSpades TwoSpades ThreeHearts ThreeSpades TwoSpades | Full House 7
-		// 3");
+		 System.out.println(r.betterHand(hs));
 		assertEquals(
 				"1 | null | FourSpades FourSpades ThreeHearts ThreeSpades FourSpades | Full House 7\n2 | null | TwoSpades TwoSpades ThreeHearts ThreeSpades TwoSpades | Full House 7",
+				r.betterHand(hs));
+	}
+	
+	@Test
+	public void testBetterHandSameHands() {
+		Round r = new Round(2);
+		Hand h1 = new Hand(new Card[] { new Card("TwoSpades"), new Card("TwoSpades"), new Card("ThreeHearts"),
+				new Card("ThreeSpades"), new Card("TwoSpades") });
+		Hand h2 = new Hand(new Card[] { new Card("TwoSpades"), new Card("TwoSpades"), new Card("ThreeHearts"),
+				new Card("ThreeSpades"), new Card("TwoSpades") });
+		Hand[] hs = new Hand[] { h1, h2 };
+		assertEquals(
+				"Tie | null | TwoSpades TwoSpades ThreeHearts ThreeSpades TwoSpades | Full House 7\nTie | null | TwoSpades TwoSpades ThreeHearts ThreeSpades TwoSpades | Full House 7",
 				r.betterHand(hs));
 	}
 
