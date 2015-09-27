f0150fede6249b17610a5f9090a59a7584827247
moose0030 <matthewpreston@hotmail.ca>
2015-09-27 00:35:55 -0400

    fixed Round test with updated verification

--- workspace/3004-A1/src/RoundTest.java
+++ workspace/3004-A1/src/RoundTest.java
@@ -3,57 +3,40 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RoundTest {
-
+	Game g = new Game();
 	@Test
 	public void testAddPlayerHand() {
 		Round r = new Round(2);
-		for (int i = 0; i < 2; i++) {
-			r.addPlayerHand(i, "Jim", new Card[] { new Card("ThreeSpades"), new Card("ThreeSpades"),
-					new Card("ThreeSpades"), new Card("ThreeSpades"), new Card("ThreeSpades") });
-		}
+		r.addPlayerHand(0, "Jim", new Card[] { g.verifyNewCard("ThreeSpades"), g.verifyNewCard("FourSpades"),g.verifyNewCard("FiveSpades"), g.verifyNewCard("SixSpades"), g.verifyNewCard("SevenSpades") });
+		r.addPlayerHand(1, "Jane", new Card[] { g.verifyNewCard("ThreeClubs"), g.verifyNewCard("FourClubs"),g.verifyNewCard("FiveClubs"), g.verifyNewCard("SixClubs"), g.verifyNewCard("SevenClubs") });
 		assertEquals(2, r.players.length);
 	}
 
 	@Test
 	public void testBetterHand() {
 		Round r = new Round(2);
-		Hand h1 = new Hand(new Card[] { new Card("TwoSpades"), new Card("TwoSpades"), new Card("ThreeHearts"),
-				new Card("ThreeSpades"), new Card("TwoSpades") });
-		Hand h2 = new Hand(new Card[] { new Card("TwoSpades"), new Card("AceSpades"), new Card("ThreeSpades"),
-				new Card("ThreeSpades"), new Card("TwoSpades") });
+		Hand h1 = new Hand(new Card[] { g.verifyNewCard("TwoHearts"), g.verifyNewCard("TwoDiamonds"), g.verifyNewCard("ThreeHearts"),g.verifyNewCard("ThreeSpades"), g.verifyNewCard("TwoClubs") });
+		Hand h2 = new Hand(new Card[] { g.verifyNewCard("FiveSpades"), g.verifyNewCard("AceSpades"), g.verifyNewCard("FourSpades"),g.verifyNewCard("SevenSpades"), g.verifyNewCard("JackSpades") });
 		Hand[] hs = new Hand[] { h1, h2 };
-		// need to fix for straigh and flush
-		System.out.println(r.betterHand(hs));
-		assertEquals(
-				"1 | null | TwoSpades TwoSpades ThreeHearts ThreeSpades TwoSpades | Full House 7\n2 | null | TwoSpades AceSpades ThreeSpades ThreeSpades TwoSpades | Flush 5",
-				r.betterHand(hs));
+		assertEquals("1 | null | TwoHearts TwoDiamonds ThreeHearts ThreeSpades TwoClubs | Full House 7\n2 | null | FiveSpades AceSpades FourSpades SevenSpades JackSpades | Flush 5",r.betterHand(hs));
 	}
 
 	@Test
 	public void testBetterHandSameRank() {
 		Round r = new Round(2);
-		Hand h1 = new Hand(new Card[] { new Card("TwoSpades"), new Card("TwoSpades"), new Card("ThreeHearts"),
-				new Card("ThreeSpades"), new Card("TwoSpades") });
-		Hand h2 = new Hand(new Card[] { new Card("FourSpades"), new Card("FourSpades"), new Card("ThreeHearts"),
-				new Card("ThreeSpades"), new Card("FourSpades") });
+		Hand h1 = new Hand(new Card[] { g.verifyNewCard("TwoSpades"), g.verifyNewCard("TwoClubs"), g.verifyNewCard("ThreeHearts"),g.verifyNewCard("ThreeSpades"), g.verifyNewCard("FiveSpades") });
+		Hand h2 = new Hand(new Card[] { g.verifyNewCard("TwoHearts"), g.verifyNewCard("TwoDiamonds"), g.verifyNewCard("ThreeClubs"),g.verifyNewCard("ThreeDiamonds"), g.verifyNewCard("FourSpades") });
 		Hand[] hs = new Hand[] { h1, h2 };
-		 System.out.println(r.betterHand(hs));
-		assertEquals(
-				"1 | null | FourSpades FourSpades ThreeHearts ThreeSpades FourSpades | Full House 7\n2 | null | TwoSpades TwoSpades ThreeHearts ThreeSpades TwoSpades | Full House 7",
-				r.betterHand(hs));
+		assertEquals("1 | null | TwoSpades TwoClubs ThreeHearts ThreeSpades FiveSpades | Two Pair 3\n2 | null | TwoHearts TwoDiamonds ThreeClubs ThreeDiamonds FourSpades | Two Pair 3",r.betterHand(hs));
 	}
 	
 	@Test
 	public void testBetterHandSameHands() {
 		Round r = new Round(2);
-		Hand h1 = new Hand(new Card[] { new Card("TwoSpades"), new Card("TwoSpades"), new Card("ThreeHearts"),
-				new Card("ThreeSpades"), new Card("TwoSpades") });
-		Hand h2 = new Hand(new Card[] { new Card("TwoSpades"), new Card("TwoSpades"), new Card("ThreeHearts"),
-				new Card("ThreeSpades"), new Card("TwoSpades") });
+		Hand h1 = new Hand(new Card[] { g.verifyNewCard("TwoSpades"), g.verifyNewCard("TwoClubs"), g.verifyNewCard("ThreeHearts"),g.verifyNewCard("ThreeSpades"), g.verifyNewCard("FiveSpades") });
+		Hand h2 = new Hand(new Card[] { g.verifyNewCard("TwoHearts"), g.verifyNewCard("TwoDiamonds"), g.verifyNewCard("ThreeClubs"),g.verifyNewCard("ThreeDiamonds"), g.verifyNewCard("FiveClubs") });
 		Hand[] hs = new Hand[] { h1, h2 };
-		assertEquals(
-				"Tie | null | TwoSpades TwoSpades ThreeHearts ThreeSpades TwoSpades | Full House 7\nTie | null | TwoSpades TwoSpades ThreeHearts ThreeSpades TwoSpades | Full House 7",
-				r.betterHand(hs));
+		assertEquals("Tie | null | TwoSpades TwoClubs ThreeHearts ThreeSpades FiveSpades | Two Pair 3\nTie | null | TwoHearts TwoDiamonds ThreeClubs ThreeDiamonds FiveClubs | Two Pair 3",r.betterHand(hs));
 	}
 
 }
